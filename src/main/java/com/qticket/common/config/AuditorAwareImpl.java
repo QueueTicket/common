9c904026fd0259edb541a5796826b8d5c2740d8c
sanghoon <tkdgns5817@gmail.com>
2024-10-10 05:18:12 +0900

    fix: auditorAware 수정

--- src/main/java/com/qticket/common/config/AuditorAwareImpl.java
+++ src/main/java/com/qticket/common/config/AuditorAwareImpl.java
@@ -2,6 +2,7 @@ package com.qticket.common.config;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -9,14 +10,35 @@ import java.util.Optional;
 
 public class AuditorAwareImpl implements AuditorAware<String> {
 
+    private static final ThreadLocal<String> asyncUserId = new ThreadLocal<>();
     @Override
     public Optional<String> getCurrentAuditor() {
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        String updatedBy = request.getHeader("X-USER-ID");
-        if (updatedBy == null) {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (requestAttributes != null) {
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+            String updatedBy = request.getHeader("X-USER-ID");
+            if (updatedBy == null) {
+                throw new SecurityException("X-USER-ID is required");
+            }
+            return Optional.of(updatedBy);
+        }
+
+        // 비동기 작업 중인 경우 ThreadLocal에서 사용자 정보 가져오기
+        String userId = asyncUserId.get();
+        if (userId == null) {
             throw new SecurityException("X-USER-ID is required");
         }
-        return Optional.of(updatedBy);
+
+        // 기본 사용자 설정
+        return Optional.of("userId");
+    }
+
+    public static void setAsyncUserId(String userId) {
+        asyncUserId.set(userId);
+    }
+
+    public static void clearAsyncUserId() {
+        asyncUserId.remove();
     }
 
 }
