f5cb53c6412e148a8979b9a7c0ea9d0d8a7894fb
sanghoon <tkdgns5817@gmail.com>
2024-10-14 03:33:21 +0900

    fix: CustomLoginAnnotation 수정

--- src/main/java/com/qticket/common/login/LoginArgumentResolver.java
+++ src/main/java/com/qticket/common/login/LoginArgumentResolver.java
@@ -11,16 +11,20 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 public class LoginArgumentResolver implements HandlerMethodArgumentResolver {
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        return parameter.hasParameterAnnotation(Login.class)  && parameter.getParameterType().equals(CurrentUser.class);
+        return parameter.hasParameterAnnotation(Login.class) && parameter.getParameterType().equals(CurrentUser.class);
     }
-
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         String userIdHeader = webRequest.getHeader("X-USER-ID");
         String userRoleHeader = webRequest.getHeader("X-USER-ROLE");
 
-        Long currentUserId = (userIdHeader != null) ? Long.parseLong(userIdHeader) : null;
+        // X-USER-ID가 null일 경우 처리
+        Long currentUserId = null;
+        if (userIdHeader != null && !userIdHeader.isEmpty()) {
+            currentUserId = Long.parseLong(userIdHeader);
+        }
+
         String currentUserRole = (userRoleHeader != null) ? userRoleHeader : null;
 
         return new CurrentUser(currentUserId, currentUserRole);
